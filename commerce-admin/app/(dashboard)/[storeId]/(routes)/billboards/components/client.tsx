5a14c5dbbcb5beb66784c6231de09262ea9c9d3d
Bryan Zheng <zhiren@umail.ucsb.edu>
2024-01-09 13:05:03 -0800

    Implemented api display on billboard page

--- commerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ commerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 
 interface BillboardClientProps {
@@ -34,6 +35,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="label" />
+            <Heading title="API" description="API calls for Billboards" />
+            <Separator />
+            <ApiList entityName="billboards" entityIdName="billboardId" />
         </>
     )
 }
