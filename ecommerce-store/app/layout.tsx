c62608610bb9d748c96e60d009602c7ec48894f8
Bryan Zheng <zhiren@umail.ucsb.edu>
2024-01-25 22:45:08 -0800

    Rename ecommerce cms folder from 'commerce-admin' to 'ecommerce-admin'

---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Ecommerce Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
